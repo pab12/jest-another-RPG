afaf1faa83daa6cc677408cecfdea91df3320c13
Paul Barcenas <paulbarcenas@yahoo.com>
2021-10-04 20:22:10 -0500

    Write getStats and getInventory Methods to Pass Test

---
+++ __tests__/potion.test.js
@@ -0,0 +1,14 @@
+const Potion = require('../lib/Potion.js');
+
+jest.mock('../lib/Potion.js');
+
+console.log(new Potion());
+
+
+test('creates a random potion object',() => {
+  const potion = new Potion();
+
+  expect(potion.name).toEqual(expect.any(String));
+  expect(potion.name.length).toBeGreaterThan(0);
+  expect(potion.value).toEqual(expect.any(Number));
+});
